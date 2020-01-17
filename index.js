05364f7c1218fcebd14b00c07f5ac0829bda665e
Emmanuel Tanifum <etantanifum@gmail.com>
2020-01-17 11:49:41 -0500

    downloaded dependencies and finished server get request

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+
+const express = require("express");
+const server = express();
+projectRouter = require('./routers/projectRouter');
+
+
+server.use(express.json());
+
+server.use('/api/projects', projectRouter)
+
+server.get('/', (req, res) => {
+    res.send("server should be working Manny")
+})
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`\n*** === Server Running on http://localhost:${port} ====***\n`);
+}); 
